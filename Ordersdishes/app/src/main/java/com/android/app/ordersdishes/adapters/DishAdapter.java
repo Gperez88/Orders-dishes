a9f0e3de7b4029240d0c7b76e4c2f7db4c5d21f9
Jalissa Trinidad <jalissa2403@gmail.com>
2015-11-23 22:38:11 -0400

    Attempt of recycler view listener

--- Ordersdishes/app/src/main/java/com/android/app/ordersdishes/adapters/DishAdapter.java
+++ Ordersdishes/app/src/main/java/com/android/app/ordersdishes/adapters/DishAdapter.java
@@ -6,9 +6,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.app.ordersdishes.R;
 import com.android.app.ordersdishes.model.Dish;
+import com.android.app.ordersdishes.model.Order;
 
 import java.text.NumberFormat;
 import java.util.List;
@@ -17,10 +19,12 @@ import java.util.List;
  * Created by gabriel on 11/1/2015.
  */
 public class DishAdapter extends RecyclerView.Adapter<DishAdapter.DishViewHolder> {
-    List<Dish> dishes;
+    private List<Dish> dishes;
+    private DishItemViewListener dishItemViewListener;
 
-    public DishAdapter(List<Dish> dishes) {
+    public DishAdapter(List<Dish> dishes,  DishItemViewListener dishItemViewListener) {
         this.dishes = dishes;
+        this.dishItemViewListener = dishItemViewListener;
     }
 
     public static class DishViewHolder extends RecyclerView.ViewHolder {
@@ -54,11 +58,34 @@ public class DishAdapter extends RecyclerView.Adapter<DishAdapter.DishViewHolder
 
     @Override
     public void onBindViewHolder(DishViewHolder holder, int position) {
-        holder.bind(dishes.get(position));
+        Dish dish = dishes.get(position);
+        holder.bind(dish);
+        Order order = new Order();
+        order.setDish(dish);
+        bindDishOrder(holder, order);
+
     }
 
     @Override
     public int getItemCount() {
         return dishes.size();
+    }
+
+    private void bindDishOrder(final DishViewHolder dishViewHolder, final Order order) {
+        if (order == null || order.getId() == -1)
+            return;
+
+
+        dishViewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dishItemViewListener.onOrderPlaced(order);
+
+            }
+        });
+
+    }
+    public interface DishItemViewListener{
+        void onOrderPlaced(Order order);
     }
 }
--- Ordersdishes/app/src/main/java/com/android/app/ordersdishes/fragments/DishFragment.java
+++ Ordersdishes/app/src/main/java/com/android/app/ordersdishes/fragments/DishFragment.java
@@ -1,24 +1,21 @@
 package com.android.app.ordersdishes.fragments;
 
-import android.app.AlertDialog;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.view.View;
-import android.widget.Button;
+import android.widget.Toast;
 
 import com.android.app.ordersdishes.R;
 import com.android.app.ordersdishes.adapters.DishAdapter;
-import com.android.app.ordersdishes.model.Dish;
 import com.android.app.ordersdishes.model.Order;
 import com.android.app.ordersdishes.service.ApiOrderDishesService;
 import com.android.app.ordersdishes.service.impl.ApiOrderDishesImpl;
-import com.gp89developers.android.commonutils.activity.dialog.ProgressDialog;
 import com.gp89developers.android.commonutils.utils.CustomAsyncTask;
 import com.gp89developers.android.commonutils.widget.recyclerview.DividerItemDecoration;
 import com.gp89developers.android.commonutils.widget.recyclerview.RecyclerEmptyView;
 
-public class DishFragment extends BaseAppListFragment<DishAdapter> {
+public class DishFragment extends BaseAppListFragment<DishAdapter> implements DishAdapter.DishItemViewListener {
     private ApiOrderDishesService apiOrderDishesService;
 
     public static DishFragment create() {
@@ -50,17 +47,7 @@ public class DishFragment extends BaseAppListFragment<DishAdapter> {
     }
 
     public DishAdapter setupAdapter() {
-        return new DishAdapter(apiOrderDishesService.getListDishes());
-    }
-
-    private void registerOrder(){
-        Order order = new Order();
-        order.setUsername(getUsername());
-        Dish dish = new Dish();
-        dish.setId(1);
-        order.setDish(dish);
-        OrderDishTask orderDishTask = new OrderDishTask();
-        orderDishTask.execute(order);
+        return new DishAdapter(apiOrderDishesService.getListDishes(), this);
     }
 
     private String getUsername(){
@@ -74,6 +61,14 @@ public class DishFragment extends BaseAppListFragment<DishAdapter> {
         }
 
         return username;
+    }
+
+    @Override
+    public void onOrderPlaced(Order order) {
+        order.setUsername(getUsername());
+        OrderDishTask orderDishTask = new OrderDishTask();
+        orderDishTask.execute(order);
+        Toast.makeText(getSupportActivity(), "Order placed", Toast.LENGTH_LONG).show();
     }
 
     private class OrderDishTask extends CustomAsyncTask<Order, Void, Boolean> {
